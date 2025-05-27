0e2b88b84229b85fa5c22ee17b7cf079830c3541
ZuHoliang <jms850615@gamil.com>
2025-05-27 21:58:37 +0800

    delete

--- springboot-jpa/src/main/java/com/example/demo/controller/RoomController.java
+++ springboot-jpa/src/main/java/com/example/demo/controller/RoomController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -58,6 +59,13 @@ public class RoomController {
 		}
 		
 		roomService.updateRoom(roomId, roomDto);
+		return "redirect:/rooms";
+	}
+	
+	@PostMapping
+	@DeleteMapping("/delete/{roomId}")
+	public String deleteRoom(@PathVariable Integer roomId) {
+		roomService.deleteRoom(roomId);
 		return "redirect:/rooms";
 	}
 
