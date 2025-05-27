f6b04afa7e9ce6ba0e94b6e51fb90b765b85f56c
ZuHoliang <jms850615@gamil.com>
2025-05-27 20:46:35 +0800

    room

--- springboot-jpa/src/main/java/com/example/demo/model/dto/RoomDto.java
+++ springboot-jpa/src/main/java/com/example/demo/model/dto/RoomDto.java
@@ -3,6 +3,7 @@ package com.example.demo.model.dto;
 import org.hibernate.validator.constraints.Range;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,7 +12,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RoomDto {
+	@NotNull(message = "房號不可為空")
+	@Range(min = 0, max = 9999, message = "房號須介於{min}~{max}")
 	private Integer roomId;
+	
+	@NotNull(message = "房間名稱不可為空")
+	@Size(min = 2, message = "房名至少要有{min}字")
 	private String roomName;
 	
 	@NotNull(message = "房間人數不可為空")
