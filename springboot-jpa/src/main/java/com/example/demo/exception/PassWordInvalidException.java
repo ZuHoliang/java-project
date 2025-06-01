0ff68851a71c08358150b96337986e66bb6a2acf
ZuHoliang <jms850615@gamil.com>
2025-06-01 21:49:44 +0800

    PassWordInvalidException

---
+++ springboot-jpa/src/main/java/com/example/demo/exception/PassWordInvalidException.java
@@ -0,0 +1,10 @@
+package com.example.demo.exception;
+
+public class PassWordInvalidException extends CertException{
+
+	public PassWordInvalidException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
